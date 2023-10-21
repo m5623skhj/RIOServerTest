5cf37fcdbd2c1300d0abb0d9b3a8a9b11aaedae0
m5623skhj <m5623skhj@gmail.com>
2023-10-21 17:39:32 +0900

    * Disconnect() : closesocket -> shutdown() 수정 * IO 카운트를 하나 줄이도록 수정

--- RIO_Test/RIOTestServer.cpp
+++ RIO_Test/RIOTestServer.cpp
@@ -468,7 +468,8 @@ void RIOTestServer::Disconnect(SessionId sessionId)
 		return;
 	}
 
-	closesocket(session->second->socket);
+	shutdown(session->second->socket, SD_BOTH);
+	IOCountDecrement(*session->second);
 }
 
 std::shared_ptr<RIOTestSession> RIOTestServer::GetNewSession(SOCKET enteredClientSocket, BYTE threadId)
